4cf673cfd6123da59be581319ef8e43a03bde3de
netonblpb <skullbog@gmail.com>
2022-02-11 15:04:29 +0300

    Task26 complete

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите степень: ");
+int B = int.Parse(Console.ReadLine());
+
+int pow = A;
+
+for (int i = 1; i < B; i++)
+{
+    pow = pow * A;
+}
+
+Console.WriteLine($"{A} в степени {B} равно {pow}");
